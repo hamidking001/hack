51ec05928fc560c19e630020c5c4886df8878e03
hamidking001 <102643076+hamidking001@users.noreply.github.com>
2022-03-30 17:24:35 +0430

    Create hack-rubika.py

---
+++ hack-rubika.py
@@ -0,0 +1,26 @@
+#hack-rubika
+
+print("###################################")
+print("|                                 |")
+print("|                                 |")
+print("|    h a c k     r u b i k a      |")
+print("|                                 |")
+print("|                                 |")
+print("|---------------------------------|")
+print("|                                 |")
+print("|   Manufacturer :    maziar      |")
+print("|                                 |")
+print("|                                 |")
+print("###################################")
+
+
+hack = input("TARGET  PHONE : ")
+
+l = ["hacking the rubika 10%...", "hacking the rubika 17%....", "hacking the rubika 68%.....", "hacking the rubika 90%......", "hacking the rubika 100%.......", " * hacking failed - Done! Crack the code  rubika target :* "]
+
+for i in l:
+    print("\r" + i, end="")
+    sleep(1.5)
+print()
+
+print("code : 6283" or "9473" or "9272")
